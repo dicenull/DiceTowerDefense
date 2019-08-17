83c905e3446787685aeb9d85008f13b7a0ebdb21
dice <dicenull.dev@gmail.com>
2019-08-17 19:50:32 +0900

    タワーの選択UIまで実装

---
+++ Assets/Scripts/TowerClickDetector.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TowerClickDetector : MonoBehaviour, IPointerClickHandler
+{
+	private TowerSelecter selecter;
+
+	private void Awake()
+	{
+		selecter = transform.parent.GetComponent<TowerSelecter>();
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		selecter.NotifyClick(this);
+	}
+}
---
+++ Assets/Scripts/TowerSelecter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSelecter : MonoBehaviour
+{
+	public void NotifyClick(MonoBehaviour sender)
+	{
+		Debug.Log(sender.name);
+	}
+}
